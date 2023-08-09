102343474f0e201c65c909cd1b00ae1eeb469e3b
Marfien <marvin.sempa.haase@gmail.com>
2023-08-09 21:31:13 +0200

    KafkaConfiguration

---
+++ controller/src/main/java/dev/minemesh/controller/configuration/KafkaConfiguration.java
@@ -0,0 +1,35 @@
+package dev.minemesh.controller.configuration;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(
+                Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress)
+        );
+    }
+
+    @Bean
+    public NewTopic registrationTopic() {
+        return new NewTopic("service-discovery:controller:register", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic unregistrationTopic() {
+        return new NewTopic("service-discovery:controller:unregister", 1, (short) 1);
+    }
+
+}
---
+++ controller/src/main/java/dev/minemesh/controller/configuration/KafkaProducerConfiguration.java
@@ -0,0 +1,38 @@
+package dev.minemesh.controller.configuration;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+@Configuration
+public class KafkaProducerConfiguration {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = Map.of(
+                BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress,
+                KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> factory) {
+        return new KafkaTemplate<>(factory);
+    }
+
+}
